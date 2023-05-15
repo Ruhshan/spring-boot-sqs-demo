5e632b4a866aa8b2b42c3e170f4c4b2a48817a90
Ruhshan <ruhshan.ahmed@gmail.com>
2023-05-15 12:23:25 +0600

    Create project

---
+++ sqsdemo/src/main/java/xyz/ruhshan/sqsdemo/SqsdemoApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruhshan.sqsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqsdemoApplication.class, args);
+    }
+
+}
---
+++ sqsdemo/src/test/java/xyz/ruhshan/sqsdemo/SqsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ruhshan.sqsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
