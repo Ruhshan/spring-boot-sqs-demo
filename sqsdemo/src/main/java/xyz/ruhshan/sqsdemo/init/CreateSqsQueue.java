81903ded7be564d51101673d00cd7cf5b08e2e67
Ruhshan <ruhshan.ahmed@gmail.com>
2023-06-28 12:42:20 +0600

    Check queues existance before creating

--- sqsdemo/src/main/java/xyz/ruhshan/sqsdemo/init/CreateSqsQueue.java
+++ sqsdemo/src/main/java/xyz/ruhshan/sqsdemo/init/CreateSqsQueue.java
@@ -2,6 +2,7 @@ package xyz.ruhshan.sqsdemo.init;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.ListQueuesResult;
 import com.amazonaws.services.sqs.model.QueueAttributeName;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @Component
 @Slf4j
@@ -21,7 +24,7 @@ public class CreateSqsQueue implements CommandLineRunner {
     public CreateSqsQueue(@Value("${spring.profiles.active}") String profile,
                           @Value("${queue.task}") String taskQueue,
                           AmazonSQSAsync amazonSQSAsync
-                          ) {
+    ) {
         this.profile = profile;
         this.amazonSQSAsync = amazonSQSAsync;
         this.taskQueue = taskQueue;
@@ -29,21 +32,33 @@ public class CreateSqsQueue implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        if(Objects.equals(profile, "dev")){
+        if (Objects.equals(profile, "dev")) {
             createQueues(taskQueue);
         }
     }
 
-    private void createQueues(String queueName){
+    private void createQueues(String queueName) throws ExecutionException, InterruptedException {
 
-        log.info("Creating queue {}", queueName);
+        Future<ListQueuesResult> listQueuesResultFuture = amazonSQSAsync.listQueuesAsync();
 
-        CreateQueueRequest createQueueRequest = new CreateQueueRequest();
-        createQueueRequest.setQueueName(queueName);
-        createQueueRequest.setAttributes(
-                Map.of(QueueAttributeName.ContentBasedDeduplication.name(),"true"));
+        ListQueuesResult listQueuesResult = listQueuesResultFuture.get();
 
-        amazonSQSAsync.createQueueAsync(createQueueRequest);
+        var isQueueExists = listQueuesResult.getQueueUrls().stream().anyMatch(url -> url.endsWith(queueName));
+
+        if (!isQueueExists) {
+
+            log.info("Creating queue {}", queueName);
+
+            CreateQueueRequest createQueueRequest = new CreateQueueRequest();
+            createQueueRequest.setQueueName(queueName);
+            createQueueRequest.setAttributes(
+                    Map.of(QueueAttributeName.ContentBasedDeduplication.name(), "true"));
+
+            amazonSQSAsync.createQueueAsync(createQueueRequest);
+
+
+        }
+
 
     }
 }
