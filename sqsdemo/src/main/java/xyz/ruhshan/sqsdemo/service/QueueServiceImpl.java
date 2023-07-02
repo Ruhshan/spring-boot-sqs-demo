103455a1a4f97ba17570bc821b8511e27b85dab6
Ruhshan <ruhshan.ahmed@gmail.com>
2023-07-02 12:28:51 +0600

    Add sleuth traceId and spanId in message header

--- sqsdemo/src/main/java/xyz/ruhshan/sqsdemo/service/QueueServiceImpl.java
+++ sqsdemo/src/main/java/xyz/ruhshan/sqsdemo/service/QueueServiceImpl.java
@@ -1,5 +1,6 @@
 package xyz.ruhshan.sqsdemo.service;
 
+import brave.Tracer;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import xyz.ruhshan.sqsdemo.dto.Task;
 
 import java.time.Instant;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -14,11 +16,13 @@ import java.util.UUID;
 public class QueueServiceImpl implements QueueService {
     private final QueueMessagingTemplate queueMessagingTemplate;
     private final String taskQueue;
+    private final Tracer tracer;
 
     public QueueServiceImpl(QueueMessagingTemplate queueMessagingTemplate,
-                            @Value("${queue.task}") String taskQueue) {
+                            @Value("${queue.task}") String taskQueue, Tracer tracer) {
         this.queueMessagingTemplate = queueMessagingTemplate;
         this.taskQueue = taskQueue;
+        this.tracer = tracer;
     }
 
     @Override
@@ -28,7 +32,13 @@ public class QueueServiceImpl implements QueueService {
         task.setArrivedAt(Instant.now());
         log.info("Publishing task to queue {}", task);
 
-        queueMessagingTemplate.convertAndSend(taskQueue, task);
+        queueMessagingTemplate.convertAndSend(taskQueue, task, headers());
 
+    }
+
+    private Map<String, Object> headers() {
+        return Map.of(
+                "SleuthTraceId",tracer.currentSpan().context().traceId(),
+                "SleuthSpanId", tracer.currentSpan().context().spanId());
     }
 }
